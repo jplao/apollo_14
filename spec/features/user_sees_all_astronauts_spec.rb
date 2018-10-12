3f2a6b0af87fc53ecb6ae90f07e32f8ea875fc16
Jenn Lao <jenn.p.lao@gmail.com>
2018-10-12 11:10:09 -0600

    spec test

--- spec/features/user_sees_all_astronauts_spec.rb
+++ spec/features/user_sees_all_astronauts_spec.rb
@@ -18,4 +18,15 @@ RSpec.describe "a visitor visits the astronaut index page" do
 
     expect(page).to have_content("Average age: 47")
   end
+
+  it 'should show an a mist of mission' do
+    neil = Astronaut.new(name: 'Neil Armstrong', age: 37, job: 'Commander')
+    mission_1 = Astronaut.missions.create(name: "Apollo 13")
+    mission_2 = Astronaut.missions.create(name: "Capricorn 4")
+    mission_3 = Astronaut.missions.create(name: "Gemini 7")
+
+    visit "/astronauts"
+
+    expect(page).to have_content(mission_1.name)
+  end
 end
